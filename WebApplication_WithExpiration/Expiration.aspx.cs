1029595a25faa76595017827fe0c2449a081848d
Finix Infosys Ltd <finixinfosysltddev@gmail.com>
2019-07-28 18:09:30 +0600

    Remove bin folder, obj folder and publishing profile; Also, modify webconfig file

--- WebApplication_WithExpiration/Expiration.aspx.cs
+++ WebApplication_WithExpiration/Expiration.aspx.cs
@@ -6,7 +6,7 @@ namespace WebApplication_WithExpiration
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            //Your code goes here
         }
     }
 }
